154907ad45b7798bb6f3991878c4611812530c4c
Gabriela Lopes <gabrieladslopes@gmail.com>
2020-06-25 19:53:03 -0300

    CRUD e rota de likes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
+
+// Middlewares
+function checkIfRepositoryExists(request, response, next) {
+  const { id } = request.params;
+
+  if (!repositories.find(repository => repository.id == id)) {
+    return response.status(400).json({ error: `Repository with id ${id} does not exist.` });
+  }
+
+  return next();
+}
 
 app.use(express.json());
 app.use(cors());
@@ -11,23 +22,54 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const newRepository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  }
+
+  repositories.push(newRepository);
+
+  return response.json(newRepository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", checkIfRepositoryExists, (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repository = repositories.find(repository => repository.id == id);
+
+  repository.title = title;
+  repository.url = url;
+  repository.techs = techs;
+
+  return response.json(repository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", checkIfRepositoryExists, (request, response) => {
+  const { id } = request.params;
+
+  const repId = repositories.findIndex(repository => repository.id == id);
+
+  repositories.splice(repId, 1);
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", checkIfRepositoryExists, (request, response) => {
+  const { id } = request.params;
+  const rep = repositories.find(repository => repository.id == id);
+
+  rep.likes += 1;
+
+  return response.json(rep);
 });
 
 module.exports = app;
